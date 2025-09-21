c0cc39dc795e8f56b3e8902157d7be218aa9353e
dallyla <moraesdallyla6@gmail.com>
2025-09-21 17:20:50 -0300

    estrutura e bd ok

---
+++ backend/src/main/java/com/bicentral/bicentral_backend/BicentralBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bicentral.bicentral_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicentralBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicentralBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bicentral/bicentral_backend/BicentralBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bicentral.bicentral_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicentralBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
