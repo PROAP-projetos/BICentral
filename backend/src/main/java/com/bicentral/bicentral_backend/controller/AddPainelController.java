6999bfa998250efc35ef9023520c458e5451137b
neci <necigalli@gmail.com>
2025-10-21 11:03:49 -0300

    testando

--- backend/src/main/java/com/bicentral/bicentral_backend/controller/AddPainelController.java
+++ backend/src/main/java/com/bicentral/bicentral_backend/controller/AddPainelController.java
@@ -7,23 +7,19 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/painel")
-// CORREÇÃO AQUI: Mude a porta para 4200 (onde o Angular está rodando)
 @CrossOrigin(origins = "http://localhost:4200")
 public class AddPainelController {
 
     @Autowired
-    // CORREÇÃO AQUI: Variáveis de instância DEVEM começar com letra minúscula (convenção Java/Spring)
     private AddPainelRepository addPainelRepository;
 
     @PostMapping
     public AddPainel criarPainel(@RequestBody AddPainel novoPainel) {
-        // Use a variável corrigida
         return addPainelRepository.save(novoPainel);
     }
 
     @GetMapping
     public java.util.List<AddPainel> listarPainel() {
-        // Use a variável corrigida
         return addPainelRepository.findAll();
     }
 }
