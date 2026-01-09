42b6b6776b65f428cba868eeb4d4d700319b8a6e
Dallyla de Moraes Sousa <133059854+dallylademoraes@users.noreply.github.com>
2026-01-09 10:30:11 -0300

    commit changes

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { authInterceptor } from './auth.interceptor'; // Importa o "carimbo" que criamos
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+providers: [
+provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
-  ]
+    // Configura o cliente HTTP para usar o interceptor de autenticação
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+)
+]
 };
